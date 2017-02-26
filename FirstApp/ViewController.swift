c86154ac2cf514362729fb3d4bddc8cd7a15c33a
Harris <Harris@harriss-mini.home>
2017-02-26 18:56:59 +0000

    test

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  //OUTLET
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    let messageArray : [String] = ["You are", "my fire", "My one", "desire", "I WANTTTT IT THAT WAY 😍"]
+    var index : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+  
+    
+    
 
-
+//ACTIONS
+    @IBAction func doButtonTap(_ sender: AnyObject) {
+        let nextString = self.messageArray[index]
+self.messageLabel.text = nextString
+ 
+    index += 1
+        index %= self.messageArray.count
+    }
 }
+
+
+
+
 
